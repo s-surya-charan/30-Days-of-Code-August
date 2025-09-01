2c37f1d14c37648d0a5acba129c6ecee1bb8fdc9
S. Surya Charan <surya7charan@gmail.com>
2025-09-01 19:41:26 +0530

    Soup Servings

--- DAY07/solution.py
+++ DAY07/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def soupServings(self, n: int) -> float:
-        # If n is very large, the probability approaches 1
         if n > 4500:
             return 1.0
         
@@ -8,24 +7,19 @@ class Solution:
         return self.solve(n, n)
 
     def solve(self, x: int, y: int) -> float:
-        # Both soups run out together
         if x <= 0 and y <= 0:
             return 0.5
         
-        # Soup A runs out first
         if x <= 0:
             return 1.0
-        
-        # Soup B runs out first
+
         if y <= 0:
             return 0.0
         
-        # Memoization key
         key = (x, y)
         if key in self.memo:
             return self.memo[key]
         
-        # Each of the 4 serving options has probability 0.25
         ans = (
             self.solve(x - 100, y) +
             self.solve(x - 75, y - 25) +
