2933514d6f174ee04fbf077399f3b85739e7c2c4
S. Surya Charan <surya7charan@gmail.com>
2025-09-01 19:31:49 +0530

    Max. Fruits Harvested

--- DAY02/solution.py
+++ DAY02/solution.py
@@ -5,7 +5,7 @@ class Solution:
             total += fruits[right][1]
             while left <= right and min(
                 abs(startPos - fruits[left][0]) + fruits[right][0] - fruits[left][0],
-                abs(startPos - fruits[right][0]) + fruits[right][0] - fruits[left][0]
+                abs(startPos - fruits[right][0]) + fruits[right][0] - fruits[left][0] 
             ) > k:
                 total -= fruits[left][1]
                 left += 1
