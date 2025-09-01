b145244e1c7391ff8ba92e4d41594e7a3d39e5a7
S. Surya Charan <surya7charan@gmail.com>
2025-09-01 18:49:19 +0530

    Pascal's Triangle

--- DAY0/solution.py
+++ DAY0/solution.py
@@ -8,4 +8,4 @@ class Solution:
             for j in range(len(res[-1]) + 1):
                 row.append(temp[j] + temp[j + 1])
             res.append(row)
-        return res
+        return res 
