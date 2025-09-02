ca59c0f78da6dc9d267189375c3877528dd7d9b4
S. Surya Charan <surya7charan@gmail.com>
2025-09-02 22:02:23 +0530

    Valid Sudoku

--- DAY29/solution.py
+++ DAY29/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)] 
         boxes = [set() for _ in range(9)]
 
         for i in range(9):
