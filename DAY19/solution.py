57e4fe2a08e97ed247f9a5926e50cf044c5bdd64
S. Surya Charan <surya7charan@gmail.com>
2025-09-02 22:07:11 +0530

    Square Submatrices

--- DAY19/solution.py
+++ DAY19/solution.py
@@ -10,7 +10,7 @@ class Solution:
             for j in range(n):
                 if matrix[i][j] == 1 and i > 0 and j > 0:
                     matrix[i][j] = min(
-                        matrix[i-1][j],
+                        matrix[i-1][j], 
                         matrix[i][j-1],
                         matrix[i-1][j-1]
                     ) + 1
